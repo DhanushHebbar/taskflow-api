8fb895a1f1ee0171bd65ed11cc53f9f56961c5da
Dhanush Hebbar <dhanushhbbr22@gmail.com>
2026-05-23 12:14:29 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,28 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['Owner', 'Admin', 'Member'],
+    default: 'Owner', // First user usually creates the workspace
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
