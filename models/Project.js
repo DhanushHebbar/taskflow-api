83d726f8ee8987bd18b5af9894ca08bb052f9a7c
Dhanush Hebbar <dhanushhbbr22@gmail.com>
2026-05-23 13:00:17 +0530

    Create Project.js

---
+++ models/Project.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const ProjectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  workspace: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Workspace',
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
